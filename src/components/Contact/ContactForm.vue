b35cf1cd323f3f1aa3acf8899fab23e58fdd0f13
Termin <67834803+Termindev@users.noreply.github.com>
2022-08-07 23:36:11 +0200

    made some modifications for the form
    (margin, removed padding)

--- src/components/Contact/ContactForm.vue
+++ src/components/Contact/ContactForm.vue
@@ -1,38 +1,31 @@
 <template>
-  <form
-    ref="form"
-    @submit.prevent="
-      sendEmail();
-      this.$refs.form.reset();
-    "
-    class="md:p-10"
-  >
-    <label class="my-8 text-2xl">
-      <span class="my-6">Full Name</span>
+  <form ref="form" @submit.prevent="sendEmail" class="mt-8">
+    <div class="mb-8 text-2xl">
+      <span>Full Name</span>
       <input
         class="rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-11/12 border-b aurora-inner outline-none border-slate-700 block"
         type="text"
         name="user_name"
         placeholder="Brendan Eich"
       />
-    </label>
-    <label class="md:my-8 text-2xl">
-      <span class="my-6">Email</span>
+    </div>
+    <div class="mb-8 text-2xl">
+      <span>Email</span>
       <input
-        class="rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-11/12 border-b outline-none aurora-inner border-slate-700 block"
+        class="text-2xl rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-11/12 border-b outline-none aurora-inner border-slate-700 block"
         type="email"
         name="user_email"
         placeholder="Brendan@email.com"
       />
-    </label>
-    <label class="md:my-8 text-2xl">
-      <span class="my-6">Message</span>
+    </div>
+    <div class="mb-8 text-2xl">
+      <span>Message</span>
       <textarea
         class="rounded hover:shadow-lg transition-all mt-4 h-20 md:w-4/5 w-11/12 aurora-inner resize-none outline-none border-b border-slate-700 block"
         name="message"
-        placeholder="Hello,&#10;I want to hire you"
+        placeholder="Hello,&#10;I wan't to hire you"
       ></textarea>
-    </label>
+    </div>
     <input
       class="cursor-pointer hover:shadow-2xl transition-all p-3 outline-none mt-1 rounded-xl aurora-inner shadow-lg block"
       type="submit"
@@ -45,9 +38,6 @@
 import emailjs from "@emailjs/browser";
 
 export default {
-  data() {
-    return {};
-  },
   methods: {
     sendEmail() {
       emailjs
