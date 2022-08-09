562a3fe449ee2c6c1d63747fa054f8e734866edc
Termin <67834803+Termindev@users.noreply.github.com>
2022-08-09 13:55:30 +0200

    Edited the text and buttons

--- src/components/WText.vue
+++ src/components/WText.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="aurora-outer" style="height: 92vh">
-    <div class="sm:flex text-5xl mx-20 pt-44 mb-12">
-      <span class="sm:mx-1 mx-4"
+    <div class="sm:flex text-5xl mx-12 sm:mx-20 pt-44 mb-12">
+      <span class="sm:mx-1"
         >Hello, I'm
 
         <VueWriter
@@ -12,12 +12,12 @@
     </div>
     <a
       href="#works"
-      class="md:ml-20 ml-8 mt-10 p-4 rounded-xl aurora-inner shadow-xl hover:shadow-2xl transition-all"
+      class="md:ml-20 sm:ml-8 ml-10 mt-10 p-4 rounded-xl aurora-inner shadow-xl hover:shadow-2xl transition-all"
       >View My work</a
     >
     <router-link
       to="/contact"
-      class="md:ml-5 ml-4 p-4 rounded-xl aurora-inner shadow-xl hover:shadow-2xl transition-all"
+      class="md:ml-5 sm:ml-4 ml-2 p-4 rounded-xl aurora-inner shadow-xl hover:shadow-2xl transition-all"
       >Hire Me</router-link
     >
   </div>
