76bfcdd785f9f3838bd3dbcb5b29d8ad16c97e14
Termin <67834803+Termindev@users.noreply.github.com>
2022-08-15 13:57:15 +0200

    fixed width of form on smal screens

--- src/components/Contact/ContactForm.vue
+++ src/components/Contact/ContactForm.vue
@@ -28,7 +28,7 @@ const sendEmail = () => {
     <div class="mb-8 text-2xl">
       <span>Full Name</span>
       <input
-        class="rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-11/12 border-b aurora-inner outline-none border-slate-700 block"
+        class="rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-full border-b aurora-inner outline-none border-slate-700 block"
         type="text"
         name="user_name"
         placeholder="Brendan Eich"
@@ -37,7 +37,7 @@ const sendEmail = () => {
     <div class="mb-8 text-2xl">
       <span>Email</span>
       <input
-        class="text-2xl rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-11/12 border-b outline-none aurora-inner border-slate-700 block"
+        class="text-2xl rounded hover:shadow-lg transition-all mt-4 md:w-4/5 w-full border-b outline-none aurora-inner border-slate-700 block"
         type="email"
         name="user_email"
         placeholder="Brendan@email.com"
@@ -46,7 +46,7 @@ const sendEmail = () => {
     <div class="mb-8 text-2xl">
       <span>Message</span>
       <textarea
-        class="rounded hover:shadow-lg transition-all mt-4 h-20 md:w-4/5 w-11/12 aurora-inner resize-none outline-none border-b border-slate-700 block"
+        class="rounded hover:shadow-lg transition-all mt-4 h-20 md:w-4/5 w-full aurora-inner resize-none outline-none border-b border-slate-700 block"
         name="message"
         placeholder="Hello,&#10;I want to hire you"
       ></textarea>
