522bc0f000619e31154d6dbfd9b1d6803621ece8
Termin <67834803+Termindev@users.noreply.github.com>
2022-08-15 20:30:27 +0200

    added cursor animation

--- src/components/WText.vue
+++ src/components/WText.vue
@@ -26,3 +26,36 @@ import VueWriter from "vue-writer";
     >
   </div>
 </template>
+<style>
+.is-typed span.cursor {
+  display: inline-block;
+  width: 3px;
+  background-color: black;
+  animation: blink 1s infinite;
+}
+
+.is-typed span.underscore {
+  display: inline-flex;
+  width: 10px;
+  height: 1px;
+  align-items: flex-end;
+  background-color: black;
+  animation: blink 1s infinite;
+}
+
+.is-typed span.cursor.typing {
+  animation: none;
+}
+
+@keyframes blink {
+  49% {
+    background-color: black;
+  }
+  50% {
+    background-color: transparent;
+  }
+  99% {
+    background-color: transparent;
+  }
+}
+</style>
