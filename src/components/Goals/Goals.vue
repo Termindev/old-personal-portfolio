c08cc76ed43ee26e2a93d4b399ed6e13a4395b96
termindev <mohamedsameh123456123@gmail.com>
2022-08-18 16:40:56 +0200

    added goals section

---
+++ src/components/Goals/Goals.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Goal from "./Goal.vue";
+</script>
+
+<template>
+  <hr />
+  <section class="aurora-outer h-screen">
+    <div class="text-center p-6">
+      <div class="text-6xl">Goals</div>
+      <div class="text-xl">Goals I want to achieve</div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+      <Goal Title="Learn Dart" :Done="true" />
+      <Goal Title="Learn Flutter" :Done="false" />
+      <Goal Title="Learn Python" :Done="false" />s
+    </div>
+  </section>
+</template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -2,6 +2,8 @@
 import WText from "@/components/WText.vue";
 import Softwares from "@/components/Softwares/Softwares.vue";
 import Works from "@/components/Works/Works.vue";
+import Goals from "@/components/Goals/Goals.vue";
+
 </script>
 
 <template>
@@ -9,5 +11,6 @@ import Works from "@/components/Works/Works.vue";
     <WText />
     <Softwares />
     <Works />
+    <Goals />
   </div>
 </template>
